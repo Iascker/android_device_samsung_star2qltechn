c45d06dc021908d88262f771d78ca597514476e6
klabit87 <keith.labit@gmail.com>
2023-09-16 10:19:49 -0500

    Initial skeleton device tree

---
+++ device.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the common OEM chipset makefile.
+$(call inherit-product, device/samsung/sdm845-common/common.mk)
+
+# Boot Animation
+TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_SCREEN_HEIGHT := 2960
+TARGET_SCREEN_WIDTH := 1440
+
+# Display
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.lcd_density=420 \
